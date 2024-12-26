901e76ee7a772915fcf43757e1618b9d6fb4da34
ankur-b <ankurbarve29@gmail.com>
2024-12-26 11:07:58 +0530

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Ankur on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
